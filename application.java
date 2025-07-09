cdc9f613dd196bbda41359e569eedcbdc5103d93
jagadeesh93461 <konetijagadeesh7@gmail.com.com>
2025-07-09 18:17:11 +0530

    new changes added

--- application.java
+++ application.java
@@ -4,3 +4,11 @@ file.write("Hello, this is a simple file example.\n")
 file.write("Python makes file handling easy!")
 file.close()
 print("File written successfully.")
+# code changed by feature222
+# read_file.py
+file = open("example.txt", "r")  # "r" = read mode
+content = file.read()
+print("File content:")
+print(content)
+file.close()
+# end with feature222
